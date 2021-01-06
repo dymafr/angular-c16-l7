bc61d3eee44db1b8a066e9a50568901fc679e008
dymafr <erwan1309@hotmail.com>
2021-01-06 21:40:57 +0100

    Initial commit

---
+++ src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Cocktail } from "../../shared/interfaces/cocktail.interface";
+import { CocktailService } from "../../shared/services/cocktail.service";
+import { PanierService } from "../../shared/services/panier.service";
+
+@Component({
+  selector: "app-cocktail-details",
+  templateUrl: "./cocktail-details.component.html",
+  styleUrls: ["./cocktail-details.component.scss"]
+})
+export class CocktailDetailsComponent implements OnInit {
+  public cocktail: Cocktail;
+
+  constructor(
+    private cocktailService: CocktailService,
+    private panierService: PanierService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.cocktailService
+        .getCocktail(+paramMap.get("index"))
+        .subscribe((cocktail: Cocktail) => {
+          this.cocktail = cocktail;
+        });
+    });
+  }
+
+  public addToPanier() {
+    this.panierService.addPanier(this.cocktail.ingredients);
+  }
+}
